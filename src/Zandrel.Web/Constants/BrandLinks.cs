f1f9baa75b23f5a2ba126026505dbea7e0824049
alejandro-briones <j.alejandro.briones.a22@gmail.com>
2026-08-24 14:22:59 -0600

    refactor: centralize brand contact links

---
+++ src/Zandrel.Web/Constants/BrandLinks.cs
@@ -0,0 +1,7 @@
+﻿namespace Zandrel.Web.Constants;
+
+public static class BrandLinks
+{
+    public const string WhatsApp =
+        "https://wa.me/527751272647?text=Hola%2C%20me%20interesa%20conocer%20m%C3%A1s%20sobre%20los%20servicios%20de%20Zandrel.";
+}
